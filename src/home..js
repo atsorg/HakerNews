aeae880a550d631b2a3474d9482269f3a2db31b7
Rahulsingh053 <80709254+Rahulsingh053@users.noreply.github.com>
2021-04-19 16:22:12 +0530

    3

--- src/home..js
+++ src/home..js
@@ -1,30 +1,30 @@
-import { useGetStories, useGetStoryById } from "./dataloader"
+import { useGetStories, useGetStoryById } from "./dataloader";
 
-
-export default function Home () {
-
-    const {data} = useGetStories()
-    // console.log(data)
-    return ( 
-        <div>
-            {data?.splice(0, 5).map(d=><Story key={d} id={d}></Story>)}
-        </div>
-    )
+export default function Home() {
+  const { data } = useGetStories();
+  // console.log(data)
+  return (
+    <div>
+      {data?.splice(0, 5).map((d) => (
+        <Story key={d} id={d}></Story>
+      ))}
+    </div>
+  );
 }
-function Story({id}) {
-    const {data} = useGetStoryById(id)
-    console.log(data)
-    return(
-        <div>
-            <h3>{data?.title}</h3>
-            {"|"}
-            <span>by &nbsp;{data?.by}</span>
-            {"|"}
-            <a href={data?.url}>&nbsp;Click Here</a>
-           {"|"}
-            <span>&nbsp;{data?.time}</span>
-            {"|"}
-            <span>&nbsp;{data?.score}</span>
-        </div>
-    )
+function Story({ id }) {
+  const { data } = useGetStoryById(id);
+  console.log(data);
+  return (
+    <div>
+      <h3>{data?.title}</h3>
+      {"|"}
+      <span>by &nbsp;{data?.by}</span>
+      {"|"}
+      <a href={data?.url}>&nbsp;Click Here</a>
+      {"|"}
+      <span>&nbsp;{data?.time}</span>
+      {"|"}
+      <span>&nbsp;{data?.score}</span>
+    </div>
+  );
 }
